53fb975520a047ab812927dcc95cafcd7ffee1f8
Ingar Solveigson Asheim <ingara@stud.ntnu.no>
2024-05-10 19:43:34 +0200

    Added macros to override STBTT_malloc and free
    Don't want the pesky stdlib malloc getting in the way of my pristine
    arenas! >:(

---
+++ src/stbtt_overrides.h
@@ -0,0 +1,27 @@
+#ifndef STBTT_OVERRIDES_H_
+#define STBTT_OVERRIDES_H_
+
+#include "isa.h"
+
+struct stbtt_alloc_ctx
+{
+    isa_arena *Arena;
+};
+
+void *
+StbttMalloc(size_t Size, stbtt_alloc_ctx *Ctx)
+{
+    return IsaArenaPush(Ctx->Arena, Size);
+}
+
+void
+StbttFree(void *Pointer, stbtt_alloc_ctx *Ctx)
+{
+    (void)Pointer;
+    IsaArenaClear(Ctx->Arena);
+}
+
+#define STBTT_malloc(x, u) StbttMalloc(x, (stbtt_alloc_ctx *)u)
+#define STBTT_free(x, u)   StbttFree(x, (stbtt_alloc_ctx *)u)
+
+#endif // STBTT_OVERRIDES_H_
